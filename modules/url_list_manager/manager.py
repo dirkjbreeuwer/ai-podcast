85081bd2b8994bb07d0f53cd3ae10bec7b0f825c
Dirk Breeuwer <87372550+dirkjbreeuwer@users.noreply.github.com>
2023-09-10 14:27:19 +0000

    Fix: URL manager to handle lists

--- modules/url_list_manager/manager.py
+++ modules/url_list_manager/manager.py
@@ -1,31 +1,38 @@
 class URLListManager:
-    def __init__(self, initial_urls=None):
-        self.urls = initial_urls if initial_urls else []
+    def __init__(self, initial_urls_file=None):
+        self.url_list = []
+        self.initial_urls_file = initial_urls_file
+        if self.initial_urls_file:
+            self.load_urls_from_file(self.initial_urls_file)
 
     def add_url(self, url):
         """Add a new URL to the list."""
-        if url not in self.urls:
-            self.urls.append(url)
+        if url not in self.url_list:
+            self.url_list.append(url)
             return f"URL {url} added successfully!"
         return f"URL {url} already exists in the list."
 
     def remove_url(self, url):
         """Remove a URL from the list."""
-        if url in self.urls:
-            self.urls.remove(url)
+        if url in self.url_list:
+            self.url_list.remove(url)
             return f"URL {url} removed successfully!"
         return f"URL {url} does not exist in the list."
 
     def get_urls(self):
         """Retrieve the list of URLs."""
-        return self.urls
+        return self.url_list
 
-    def load_urls_from_file(self, file_path):
+    def load_urls_from_file(self, file_path=None):
         """Load URLs from a file."""
+        if not file_path:
+            file_path = self.initial_urls_file
         with open(file_path, 'r') as file:
             lines = file.readlines()
             for line in lines:
                 self.add_url(line.strip())
+        return self.url_list
+
 
     def save_urls_to_file(self, file_path="data/raw/initial_urls.txt"):
         """Save URLs to a file."""
